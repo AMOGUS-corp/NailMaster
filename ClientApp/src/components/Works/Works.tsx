0fbedf41370dac86743cb6a9ef5e156c7fdb08f4
Dmitriy <kdima18@mail.ru>
2022-05-14 20:23:17 +0300

    slider component

--- ClientApp/src/components/Works/Works.tsx
+++ ClientApp/src/components/Works/Works.tsx
@@ -1,20 +1,27 @@
-import React, { FC } from 'react'
-import './works.scss'
+import React, { FC } from "react";
+import Slider from "../common/Slider/Slider";
+import "./works.scss";
+const image = require('../../assets/images/img1.jpg')
 
 const Works: FC = () => {
-	return (
-		<section id='works' className='works'>
-			<div className="container">
-				<h2 className='title'>Мои работы</h2>
-				<div className="works__gallery">
-					<div className="works__img"></div>
-					<div className="works__img"></div>
-					<div className="works__img"></div>
-					<div className="works__img"></div>
-				</div>
-			</div>
-		</section>
-	)
-}
+  return (
+    <section id='works' className='works'>
+    	<div className="container">
+    		<h2 className='title'>Мои работы</h2>
+    		{/* <div className="works__gallery">
+    			<div className="works__img"></div>
+    			<div className="works__img"></div>
+    			<div className="works__img"></div>
+    			<div className="works__img"></div>
+    		</div> */}
+				<Slider>
+					<img src={image} />
+					<img src={image} />
+					<img src={image} />
+				</Slider>
+    	</div>
+    </section>
+  );
+};
 
-export default Works
+export default Works;
---
+++ ClientApp/src/components/common/Slider/Slider.tsx
@@ -0,0 +1,42 @@
+import React, { FC, useEffect, useRef, useState } from 'react'
+import { JsxChild } from 'typescript'
+import './slider.scss'
+const image = require('../../../assets/images/img1.jpg')
+
+interface SliderProps{
+	children: React.ReactNode
+}
+
+const Slider: FC<SliderProps> = ({children}) => {
+	const [slidesCount, setSlidesCount] = useState(0)
+	const [currentSlide, setCurrentSlide] = useState(0)
+	const slideRef = useRef<HTMLDivElement>(null)
+
+	const handleSlideBtn = (num:number) => {
+		if(num + currentSlide > slidesCount-1){
+			setCurrentSlide(0)
+			return
+		}
+		if(num + currentSlide < 0){
+			setCurrentSlide(slidesCount-1)
+			return
+		}
+		setCurrentSlide(prev => prev + num)
+	}
+
+	useEffect(() => {
+		setSlidesCount(slideRef.current?.childNodes.length || 0)
+	}, [])
+
+	return (
+		<div className='slider'>
+			<div ref={slideRef} className="slider__line" style={{left: `-${currentSlide}00%`}}>
+				{children}
+			</div>
+			<div onClick={() => handleSlideBtn(-1)} className="slider__btn slider__btn-left"></div>
+			<div onClick={() => handleSlideBtn(1)} className="slider__btn slider__btn-right"></div>
+		</div>
+	)
+}
+
+export default Slider
